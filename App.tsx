58e1de1d8d127ffd026f60f1b87769806900b282
Weverson-Luan <wltech2022@gmail.com>
2023-04-05 22:36:25 -0300

    setup :rocket: feito setup do projeto.

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const app = () => {
+  return (
+    <View>
+      <Text>oii</Text>
+    </View>
+  );
+};
+
+export { app as App };
